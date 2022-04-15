d1874fae986ddbf8c08e5754451b70461c3ca67e
Dheeraj <deebisht07@gmail.com>
2022-04-16 02:08:40 +0530

    initial

--- App.js
+++ App.js
@@ -1,10 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import SingleTodo from "./components/SingleTodo";
 
 export default function App() {
+  const [todo, setTodo] = useState("");
+  const [Todos, setTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    if (!todo) return;
+    setTodos([...Todos, { id: Date.now(), text: todo }]);
+    setTodo("");
+  };
+  const fetchTodos = async () => {
+    const data = await AsyncStorage.getItem("todos");
+    if (data) setTodos(JSON.parse(data));
+  };
+  useEffect(() => {
+    fetchTodos();
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.app_heading}>TASKER</Text>
+      <View style={styles.todo_container}>
+        <TextInput
+          placeholder="Enter a todo"
+          value={todo}
+          style={styles.input}
+          onChangeText={(e) => setTodo(e)}
+        />
+        <TouchableOpacity
+          activeOpacity={0.3}
+          style={styles.add}
+          onPress={handleAddTodo}
+        >
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>Add</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          width: "100%",
+        }}
+      >
+        <FlatList
+          style={styles.all_todos}
+          data={Todos}
+          renderItem={({ item }) => (
+            <SingleTodo Todo={item} Todos={Todos} setTodos={setTodos} />
+          )}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +67,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: "#121212",
+    alignItems: "center",
+    textTransform: "uppercase",
+    fontFamily: "Roboto",
+  },
+  app_heading: {
+    marginTop: 50,
+    fontSize: 50,
+    fontWeight: "700",
+    color: "#fff",
+  },
+  todo_container: {
+    marginTop: 30,
+    marginBottom: 30,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "#fff",
+    padding: 12,
+    width: "60%",
+    color: "#000",
+    borderRadius: 10,
+  },
+  add: {
+    marginLeft: 10,
+    paddingVertical: 12,
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "15%",
+    backgroundColor: "#0094ff",
+    borderRadius: 10,
+  },
+  all_todos: {
+    flexDirection: "column",
+    width: "100%",
+    paddingHorizontal: 20,
   },
 });
---
+++ components/SingleTodo.js
@@ -0,0 +1,113 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export default function SingleTodo({ Todo, Todos, setTodos }) {
+  const [edit, setEdit] = useState(false);
+  const [editedTodo, setEditedTodo] = useState(Todo.text);
+
+  const handleTodoDelete = () => {
+    setTodos(Todos.filter((todo) => todo.id !== Todo.id));
+    AsyncStorage.setItem("todos", JSON.stringify(Todos));
+  };
+  const handleTodoEdit = () => {
+    if (!edit) setEdit(!edit);
+    else {
+      setEdit(!edit);
+      setTodos(
+        Todos.map((todo) =>
+          todo.id === Todo.id ? { id: Todo.id, text: editedTodo } : todo
+        )
+      );
+      AsyncStorage.setItem("todos", JSON.stringify(Todos));
+    }
+  };
+
+  useEffect(() => {
+    AsyncStorage.setItem("todos", JSON.stringify(Todos));
+  }, [Todos]);
+
+  return (
+    <View style={styles.todo}>
+      {edit ? (
+        <TextInput
+          value={editedTodo}
+          style={styles.edit_todo}
+          onChangeText={(e) => setEditedTodo(e)}
+        />
+      ) : (
+        <Text style={styles.todo_text}>{Todo.text}</Text>
+      )}
+
+      <View style={styles.actions}>
+        {edit ? (
+          <TouchableOpacity onPress={handleTodoEdit}>
+            <MaterialIcons
+              name="save"
+              size={25}
+              color="white"
+              style={styles.edit_action}
+            />
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={handleTodoEdit}>
+            <MaterialIcons
+              name="edit"
+              size={25}
+              color="white"
+              style={styles.edit_action}
+            />
+          </TouchableOpacity>
+        )}
+
+        <TouchableOpacity onPress={handleTodoDelete}>
+          <MaterialIcons
+            name="delete"
+            size={25}
+            color="white"
+            style={styles.delete_action}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  todo: {
+    flexDirection: "row",
+    width: "100%",
+    backgroundColor: "#0094ff",
+    borderRadius: 10,
+    marginVertical: 10,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  todo_text: {
+    color: "#fff",
+  },
+  edit_todo: {
+    backgroundColor: "#fff",
+    width: "70%",
+    height: 30,
+    borderRadius: 10,
+    paddingLeft: 10,
+  },
+  actions: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  edit_action: {
+    marginHorizontal: 10,
+  },
+  delete_action: {
+    marginHorizontal: 10,
+  },
+});
